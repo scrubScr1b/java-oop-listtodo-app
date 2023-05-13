130aa35a86cedcdd19d3aaaa216d388854b87292
scrubScr1b <ciwifc24@gmail.com>
2023-05-13 09:10:05 +0700

    Initial Project

---
+++ java_oop_todolist/src/main/java/java/oop/todolist/App.java
@@ -0,0 +1,13 @@
+package java.oop.todolist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
